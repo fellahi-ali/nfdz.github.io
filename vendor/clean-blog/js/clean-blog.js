4e1a2ff8bae8ea0c93418e7ad1db2580a7881da4
nfdz <noe.fernandez@protonmail.com>
2017-06-07 15:29:09 +0200

    fix navigation bar and change fonts

--- vendor/clean-blog/js/clean-blog.js
+++ vendor/clean-blog/js/clean-blog.js
@@ -11,30 +11,35 @@ $(function() {
 
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
-    var MQL = 1170;
-
     //primary navigation slide-in effect
-    if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height();
-        $(window).on('scroll', {
-                previousTop: 0
-            },
-            function() {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
-                } else if (currentTop > this.previousTop) {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-                }
-                this.previousTop = currentTop;
-            });
-    }
+    var MQL = 768;
+    var headerHeight = $('.navbar-custom').height();
+    $(window).on('scroll', {
+            previousTop: 0
+        },
+        function() {
+            var hasToggleMenu = $(window).width() <= MQL;
+            var ignore = false;
+            if (hasToggleMenu == true) {
+                var isOpen = $("#navbar-collapse-1").hasClass("in");
+                ignore = isOpen;
+            }
+            if (ignore == false) {
+              var currentTop = $(window).scrollTop();
+              //check if user is scrolling up
+              if (currentTop < this.previousTop) {
+                  //if scrolling up...
+                  if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                      $('.navbar-custom').addClass('is-visible');
+                  } else {
+                      $('.navbar-custom').removeClass('is-visible is-fixed');
+                  }
+              } else if (currentTop > this.previousTop) {
+                  //if scrolling down...
+                  $('.navbar-custom').removeClass('is-visible');
+                  if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+              }
+            }
+            this.previousTop = currentTop;
+        });
 });
